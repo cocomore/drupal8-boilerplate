8372db5947a19cf6d8106cd5291413aa30280cf6
jlbellido <jose.bellido@cocomore.com>
2016-01-13 20:50:34 +0100

    Adapt setup.sh script

--- scripts/setup/001_setup_drupal.sh
+++ scripts/setup/001_setup_drupal.sh
@@ -7,17 +7,18 @@ fi
 
 cd $HTDOCS/sites/default/
 
-if [ ! -f "settings.php" ]; then
-  # No drupal installed dowload it and place it in the correct directory.
-  cp default.settings.php settings.php
-  cat $ROOT_DIR/conf/drupal/$ENVIRONMENT/setup.conf >> settings.php
-fi
+# Update the settings.php file if we added something new.
+cp default.settings.php settings.php
+cat $ROOT_DIR/conf/drupal/$ENVIRONMENT/setup.conf >> settings.php
 
 if [ ! -f "services.yml" ]; then
   cp default.services.yml services.yml
 fi
 
-if [ ! -h "$HTDOCS/profiles/cocomore" ]; then
+# Link the profile folder with the drupal profile one.
+if [ -h "$HTDOCS/profiles/cocomore" ]; then
   # Link the profiles directory with our cocomore profile.
-  ln -s $HTDOCS/../profiles/cocomore $HTDOCS/profiles/cocomore
+  unlink "$HTDOCS/profiles/cocomore"
 fi
+
+ln -s $HTDOCS/../profiles/cocomore $HTDOCS/profiles/cocomore
--- scripts/setup/020_drush_siteinstall.sh
+++ scripts/setup/020_drush_siteinstall.sh
@@ -9,4 +9,4 @@ echo "Drush location: $(which $DRUSHBIN)"
 echo "Drush version: $($DRUSHBIN --version)"
 
 $DRUSHBIN sql-drop --yes --debug
-XDEBUG_CONFIG="idekey=phpstorm" $DRUSHBIN site-install cocomore_profile --locale=de --yes --site-name="Cocomore Drupal 8"
+$DRUSHBIN site-install cocomore_profile --locale=de --yes --site-name="Cocomore Drupal 8"
