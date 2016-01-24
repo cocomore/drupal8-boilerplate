731ee7e9df8a19ab96f29b1f01740faf0246cec4
jlbellido <jose.bellido@cocomore.com>
2016-01-24 12:10:21 +0100

    Fix translations import step.

--- scripts/update/800_translation.sh
+++
@@ -1,8 +0,0 @@
-# ----------------------------------------
-# Update available contrib translations
-# ----------------------------------------
-for translation in `ls $ROOT_DIR/translations/*.po`
-do
-  $DRUSHBIN --include=$ROOT_DIR/modules/contrib/drush_language/ language-import de $translation --replace
-  echo "Translation $translation imported."
-done
